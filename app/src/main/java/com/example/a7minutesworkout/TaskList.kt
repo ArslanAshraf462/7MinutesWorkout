46f431b3ef26f6190b8b954ed319730efb2813ea
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-13 11:29:50 +0500

    Added -> viewbinding with recyclerviews

---
+++ app/src/main/java/com/example/a7minutesworkout/TaskList.kt
@@ -0,0 +1,15 @@
+package com.example.a7minutesworkout
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", timestamp = "7:00 am"),
+        Task("Eat BreakFast", timestamp = "8:00 am"),
+        Task("Attend a Meeting", timestamp = "9:00 am"),
+        Task("Read a Book", timestamp = "12:00 pm"),
+        Task("Eat Lunch", timestamp = "2:00 pm"),
+        Task("Buy Groceries", timestamp = "4:00 am"),
+        Task("Watch Youtube Videos", timestamp = "6:00 am"),
+        Task("Eat Dinner", timestamp = "7:00 am"),
+        Task("Go to Bed", timestamp = "9:00 pm"),
+    )
+}
