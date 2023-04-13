18c29e30c9a251c0da30487108c5730e7660caab
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-13 13:53:27 +0500

    Added -> displaying at witch exercise we currently are

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -117,6 +117,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
            override fun onFinish() {
                currentExercisePosition++
+               exerciseList!![currentExercisePosition].setIsSelected(true)
+               exerciseAdapter!!.notifyDataSetChanged()
                setupExerciseView()
 //               Toast.makeText(
 //                   this@ExerciseActivity,
@@ -138,6 +140,9 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
 
             override fun onFinish() {
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
                 if(currentExercisePosition < exerciseList?.size!! - 1){
                     setupRestView()
                 }else{
