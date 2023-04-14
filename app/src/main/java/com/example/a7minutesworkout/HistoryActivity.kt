f8d22f3128c67a754bbc6fc43191bf01353c837e
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-14 13:22:07 +0500

    Added -> the history ui and activity

---
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+    private var binding : ActivityHistoryBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_history)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarHistoryActivity)
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "HISTORY"
+        }
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
