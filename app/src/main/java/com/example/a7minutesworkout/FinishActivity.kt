afbfc5f0c609261da61303b203506b03b4cb6ab6
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-18 14:50:55 +0500

    Added -> the room database to our workout app - Inserting Workouts

--- app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -2,7 +2,12 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     //Create a binding variable
@@ -25,6 +30,22 @@ class FinishActivity : AppCompatActivity() {
         // Adding a click event to the Finish Button
         binding?.btnFinish?.setOnClickListener {
             finish()
+        }
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        addDateToDatabase(historyDao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date: ","" +dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date: ","" +date)
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
         }
     }
 }
