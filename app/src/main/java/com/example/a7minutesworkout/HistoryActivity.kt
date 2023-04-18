301b86c7d40fd114d2fc3460cf7f3c436b727341
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-18 15:13:35 +0500

    Added -> fetching data from our database and displaying it in the log

--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -2,7 +2,10 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding : ActivityHistoryBinding? = null
@@ -19,5 +22,22 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        getAllCompletedDates(historyDao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao){
+        lifecycleScope.launch{
+            historyDao.fetchAllDates().collect{ allCompletedDatedList ->
+                for (i in allCompletedDatedList){
+                    Log.e("Date: ", "" +i.date)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
